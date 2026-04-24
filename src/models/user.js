f13d2eeeff65b45ed5e2221b27eafd8da2d43d63
Alina <alinazolotopup99@gmail.com>
2026-04-24 17:13:29 +0200

    add user session model

--- src/models/user.js
+++ src/models/user.js
@@ -1,11 +1,30 @@
 import { Schema, model } from 'mongoose';
+import { GENDER } from '../constatnts/gender';
+
 
 const userScema = new Schema(
-  {},
+  {
+  username: { type: String, trim: true },
+   email: { type: String, unique: true, required: true, trim: true },
+   password: { type: String, required: true },
+   data: { type: Number, required: true },
+   gender: { type: String, enum: GENDER},
+   avatar: {
+     type: String,
+     required: false,
+     default: 'https://ac.goit.global/fullstack/react/default-avatar.jpg',
+   },
+},
   {
     timestamps: true,
     versionKey: false,
   },
 );
+
+userScema.methods.toJSON = function () {
+ const obj = this.toObject();
+ delete obj.password;
+ return obj;
+};
 
 export const User = model('User', userScema);
