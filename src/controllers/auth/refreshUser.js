82dad364d833667a47b6da2c17ce27cfddfe8bca
oksanA016 <frunzeoksana86@gmail.com>
2026-04-25 13:25:46 +0300

    made 18 and 19 tasks

--- src/controllers/auth/refreshUser.js
+++ src/controllers/auth/refreshUser.js
@@ -1,1 +1,54 @@
-export async function refreshUser(req, res) {}
+import jwt from "jsonwebtoken";
+import createHttpError from "http-errors";
+import { User } from "../../models/user.js";
+
+const JWT_SECRET = process.env.JWT_SECRET;
+const JWT_REFRESH_SECRET = process.env.JWT_REFRESH_SECRET;
+
+export async function refreshUser(req, res, next) {
+  try {
+    const { refreshToken } = req.cookies;
+
+    if (!refreshToken) {
+      throw createHttpError(401, "Refresh token not found");
+    }
+
+    let decoded;
+
+    try {
+      decoded = jwt.verify(refreshToken, JWT_REFRESH_SECRET);
+    } catch (err) {
+      throw createHttpError(401, "Invalid refresh token");
+    }
+
+    const user = await User.findById(decoded.id);
+
+    if (!user) {
+      throw createHttpError(401, "User not found");
+    }
+
+    const newAccessToken = jwt.sign(
+      { id: user._id },
+      JWT_SECRET,
+      { expiresIn: "15m" }
+    );
+
+    const newRefreshToken = jwt.sign(
+      { id: user._id },
+      JWT_REFRESH_SECRET,
+      { expiresIn: "7d" }
+    );
+
+    res.cookie("refreshToken", newRefreshToken, {
+      httpOnly: true,
+      secure: false,
+      sameSite: "strict",
+    });
+
+    res.json({
+      accessToken: newAccessToken,
+    });
+  } catch (error) {
+    next(error);
+  }
+}
--- src/controllers/user/getUser.js
+++ src/controllers/user/getUser.js
@@ -1,1 +1,13 @@
-export async function getUser(req, res) {}
+import createHttpError from "http-errors";
+
+export async function getUser(req, res, next) {
+  try {
+    if (!req.user) {
+      throw createHttpError(401, "Not authorized");
+    }
+
+    res.json(req.user);
+  } catch (error) {
+    next(error);
+  }
+}
