267b1e452e60e2a3fadf4358a17f4d060f173357
Maksim_Regeda <maxymregeda669@gmail.com>
2026-04-26 01:50:49 +0300

    delete/get

--- src/controllers/note/deleteNote.js
+++ src/controllers/note/deleteNote.js
@@ -1,1 +1,15 @@
-export async function deleteNote(req, res) {}
+import createHttpError from "http-errors";
+import { Note } from "../../models/note.js";
+
+export async function deleteNote(req, res) { 
+    const { noteId } = req.params;
+    const note = await Note.findOneAndDelete({
+        _id: noteId,
+    });
+
+    if (!note) {
+        throw createHttpError(404, "Note not found");
+    }
+
+    res.status(200).json(note);
+};
