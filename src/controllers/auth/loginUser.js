c43259efd7d525aad1d567dd731a4085cb7a9ca9
George Riabov <george.riabov3105@gmail.com>
2026-04-25 08:51:06 +0200

    feat: add login and register controllers

--- src/controllers/auth/loginUser.js
+++ src/controllers/auth/loginUser.js
@@ -1,1 +1,24 @@
-export async function loginUser(req, res) {}
+export const loginUser = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      throw createHttpError(401, 'Invalid credentials');
+    }
+
+    const isValidPassword = await bcrypt.compare(password, user.password);
+    if (!isValidPassword) {
+      throw createHttpError(401, 'Invalid credentials');
+    }
+
+    await Session.deleteMany({ userId: user._id });
+
+    const newSession = await createSession(user._id);
+    setSessionCookies(res, newSession);
+
+    res.status(200).json(user);
+  } catch (err) {
+    next(err);
+  }
+};
--- src/controllers/auth/registerUser.js
+++ src/controllers/auth/registerUser.js
@@ -1,1 +1,25 @@
-export async function registerUser(req, res) {}
+export const registerUser = async (req, res, next) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      throw createHttpError(400, 'Email in use');
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    const newSession = await createSession(newUser._id);
+    setSessionCookies(res, newSession);
+
+    res.status(201).json(newUser);
+  } catch (err) {
+    next(err);
+  }
+};
