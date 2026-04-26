bbb5efc599b63137a8477911c67ce4369bf28a5e
Olga Demchenko <olga.tildastyle@gmail.com>
2026-04-26 23:27:21 +0300

    add auth validation and baby state controller

--- src/controllers/home/babyState.js
+++ src/controllers/home/babyState.js
@@ -1,1 +1,24 @@
-export async function babyState(req, res) {}
+import BabyState from '../../models/babyState.js';
+import { getCurrentWeek } from '../../services/term.js';
+
+export const getBabyState = async (req, res, next) => {
+  try {
+    const user = req.user;
+
+    if (!user || !user.dueDate) {
+      return res.status(400).json({ message: 'No due date provided' });
+    }
+
+    const currentWeek = getCurrentWeek(new Date(user.dueDate));
+
+    const babyState = await BabyState.findOne({ week: currentWeek });
+
+    if (!babyState) {
+      return res.status(404).json({ message: 'Baby state not found' });
+    }
+
+    res.json(babyState);
+  } catch (error) {
+    next(error);
+  }
+};
