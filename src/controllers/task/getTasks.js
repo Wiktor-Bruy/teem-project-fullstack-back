66d0f75d00322ba461d6cea7f459943aeab3c131
Crazy_Fox <holmyliszag@ukr.net>
2026-04-26 19:03:14 +0300

    fix status response

--- src/controllers/task/getTasks.js
+++ src/controllers/task/getTasks.js
@@ -6,7 +6,7 @@ export async function getTasks(req, res) {
       createdAt: -1,
     });
 
-    res.json(tasks);
+    res.status(200).json(tasks);
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
