407532e1e02d64980c314ed5e5d72125361506a4
Vadym <vadimkiricenko572@gmail.com>
2026-04-26 22:19:36 +0300

    task24: add updateTaskStatus controller

--- src/controllers/task/updateTaskStatus.js
+++ src/controllers/task/updateTaskStatus.js
@@ -1,1 +1,22 @@
-export async function updateTaskStatus(req, res) {}
+import { Task } from '../../models/task.js';
+
+export async function updateTaskStatus(req, res) {
+  try {
+    const { id } = req.params;
+    const { status } = req.body;
+
+    const task = await Task.findOneAndUpdate(
+      { _id: id, owner: req.user._id },
+      { status },
+      { new: true }
+    );
+
+    if (!task) {
+      return res.status(404).json({ message: 'Task not found' });
+    }
+
+    res.status(200).json(task);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+}
