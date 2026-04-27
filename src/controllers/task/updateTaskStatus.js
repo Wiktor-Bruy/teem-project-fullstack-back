18e2c97f84de11cf0f52bf655d5b94291bb9ee4f
Crazy_Fox <holmyliszag@ukr.net>
2026-04-27 11:47:13 +0300

    fix update status

--- src/controllers/task/updateTaskStatus.js
+++ src/controllers/task/updateTaskStatus.js
@@ -1,22 +1,23 @@
+import createHttpError from 'http-errors';
 import { Task } from '../../models/task.js';
 
 export async function updateTaskStatus(req, res) {
-  try {
-    const { taskId } = req.params;
-    const { status } = req.body;
-
-    const task = await Task.findOneAndUpdate(
-      { _id: taskId, owner: req.user._id },
-      { status },
-      { returnDocument: 'after' },
-    );
-
-    if (!task) {
-      return res.status(404).json({ message: 'Task not found' });
-    }
-
-    res.status(200).json(task);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
+  const { taskId } = req.params;
+  const oldTask = Task.findOne({ _id: taskId, owner: req.user._id });
+  if (!oldTask) {
+    throw createHttpError(404, 'Task not found');
   }
+  const status = oldTask.isDone ? false : true;
+
+  const task = await Task.findOneAndUpdate(
+    { _id: taskId, owner: req.user._id },
+    { isDone: status },
+    { returnDocument: 'after' },
+  );
+
+  if (!task) {
+    throw createHttpError(404, 'Task not found');
+  }
+
+  res.status(200).json(task);
 }
