07096a346812ebf6cbeb08e6345548926630572d
Crazy_Fox <holmyliszag@ukr.net>
2026-04-27 14:12:11 +0300

    add get emotions route

--- src/server.js
+++ src/server.js
@@ -14,6 +14,7 @@ import userRouter from './routes/userRoutes.js';
 import taskRouter from './routes/taskRoutes.js';
 import noteRouter from './routes/noteRoutes.js';
 import homeRouter from './routes/homeRoutes.js';
+import emotionRouter from './routes/emotionRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
@@ -28,6 +29,7 @@ app.use(userRouter);
 app.use(taskRouter);
 app.use(noteRouter);
 app.use(homeRouter);
+app.use(emotionRouter);
 
 app.use(notFoundHandler);
 app.use(errors());
